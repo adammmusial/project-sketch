545ec88f0798c77af79fbfe3e65e3c6ec568473a
adammmusial <musialadam83@gmail.com>
2024-03-05 19:54:41 +0800

    feat: append divs to container

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ var button = document.getElementById('clearButton');
 function addDivs(numberOfDivsToCreate) {
     var gridCellDimension = ((600 / numberOfDivsToCreate) -2).toFixed(2);
     var gridSize = Math.pow(numberOfDivsToCreate,2);
+
+
+    while (gridSize >0) {
+        var newDiv = document.createElement('div');
+        container.appendChild(newDiv)
+        newDiv.classList.add('grid')
+    }
 }
