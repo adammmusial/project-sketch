940029dcf66e8a5845e0ebfc9010eac0770e555a
adammmusial <musialadam83@gmail.com>
2024-03-05 19:44:00 +0800

    feat: add variables for calculating grid size and dimension

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Herllo world")
+var container = document.querySelector('.container'); 
+var button = document.getElementById('clearButton'); 
+
+
+function addDivs(numberOfDivsToCreate) {
+    var gridCellDimension = ((600 / numberOfDivsToCreate) -2).toFixed(2);
+    var gridSize = Math.pow(numberOfDivsToCreate,2);
+}
