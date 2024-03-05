0f91c5ed0e1448721003b92a0120652b1290c0ee
adammmusial <musialadam83@gmail.com>
2024-03-05 20:14:19 +0800

    feat: add function clear() to clear the grid

--- js/index.js
+++ js/index.js
@@ -1,31 +1,43 @@
-var container = document.querySelector('.container'); 
-var button = document.getElementById('clearButton'); 
+var container = document.querySelector('.container');
+var button = document.getElementById('clearButton');
 
 
 function addDivs(numberOfDivsToCreate) {
-    var gridCellDimension = ((600 / numberOfDivsToCreate) -2).toFixed(2);
-    var gridSize = Math.pow(numberOfDivsToCreate,2);
+    var gridCellDimension = ((600 / numberOfDivsToCreate) - 2).toFixed(2);
+    var gridSize = Math.pow(numberOfDivsToCreate, 2);
 
 
-    while (gridSize >0) {
+    while (gridSize > 0) {
         var newDiv = document.createElement('div');
         container.appendChild(newDiv)
         newDiv.classList.add('grid')
         newDiv.style.height = gridCellDimension + 'px';
         newDiv.style.width = gridCellDimension + 'px';
         newDiv.style.border = '1px solid black';
-        }
-    
+    }
+
     var gridCells = document.querySelectorAll('.grid')
-    gridCells.forEach(cell => cell.addEventListener('mouseenter',changeColor));
+    gridCells.forEach(cell => cell.addEventListener('mouseenter', changeColor));
     console.log(gridCellDimensions);
     console.log('Grid successfully created!')
-    
+
 }
 
 function changeColor() {
     this.styl.backgroundColor = '#ff9999'
 }
 
-button.addEventListener('click', clear); 
+function clear() {
+    var reqGridSize = prompt('How many squares per side?')
+    if (reqGridSize >= 1 && reqGridSize <= 100) {
+        while (container.hasChildNodes()) {
+            container.removeChild(container.lastChild);
+        }
+        addDiv(reqGridSize)
+    } else {
+        alert('Choose a number between 1 and 100');
+    }
+}
+
+button.addEventListener('click', clear);
 window.onload = addDiv(16); 
